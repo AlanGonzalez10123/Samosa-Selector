b78e7e41eef3c94b6aadad90a835a32d2df80f44
AlanGonzalez10123 <alangonzalezosorio04@gmail.com>
2024-09-24 21:12:50 -0400

    added multipliers

--- SamosaSelector/src/App.jsx
+++ SamosaSelector/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
   const [clicks, setClicks] = useState(0)
   const [multiplier, setMultiplier] =  useState(1)
 
-  const handleClick = () => {setClicks(clicks + 1) }
+  const handleClick = () => { setClicks(clicks + multiplier) }
+
+  const handleMultiplier = (multiplier) => { setMultiplier(multiplier) }
 
   return (
     <div className="App">
@@ -15,6 +17,26 @@ function App() {
       </div>
       <div className="Clicker">
         <p>{clicks}</p>
+      </div>
+
+      <div className="Multipliers">
+        <div className="two">
+          <h3>Double Stuffed</h3>
+          <h5>2x Multiplier</h5>
+          <button onClick={() => handleMultiplier(2)} >10 samosas</button>
+        </div>
+
+        <div className="five">
+          <h3>Party Pack</h3>
+          <h5>5x Multiplier</h5>
+          <button onClick={() => handleMultiplier(5)}>100 samosas</button>
+        </div>
+
+        <div className="ten">
+          <h3>Full Feast</h3>
+          <h5>10x Multiplier</h5>
+          <button onClick={() => handleMultiplier(10)} >1000 samosas</button>
+        </div>
       </div>
 
 

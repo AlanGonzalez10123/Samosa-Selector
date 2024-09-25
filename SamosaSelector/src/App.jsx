2e1b8d8c8132fabbe21dd653f27f95e0e99b06b0
AlanGonzalez10123 <alangonzalezosorio04@gmail.com>
2024-09-24 21:30:42 -0400

    buttons disapear after one use

--- SamosaSelector/src/App.jsx
+++ SamosaSelector/src/App.jsx
@@ -4,10 +4,44 @@ import './App.css'
 function App() {
   const [clicks, setClicks] = useState(0)
   const [multiplier, setMultiplier] =  useState(1)
+  const [twoFlag, setTwoFlag] = useState(true)
+  const [fiveFlag, setFiveFlag] = useState(true)
+  const [tenFlag, setTenFlag] = useState(true)
 
   const handleClick = () => { setClicks(clicks + multiplier) }
 
-  const handleMultiplier = (multiplier) => { setMultiplier(multiplier) }
+  
+
+  function doubler(){
+    if (clicks >= 10)
+    {
+      setClicks(clicks - 10)
+      setMultiplier(2)
+      setTwoFlag(false)
+    }
+
+  }
+
+  function fiveX(){
+    if (clicks >= 100)
+    {
+      setClicks(clicks - 100)
+      setMultiplier(5)
+      setFiveFlag(false)
+    }
+
+  }
+
+  function tenX(){
+    if (clicks >= 1000)
+    {
+      setClicks(clicks - 1000)
+      setMultiplier(10)
+      setTenFlag(false)
+    }
+
+  }
+
 
   return (
     <div className="App">
@@ -16,26 +50,26 @@ function App() {
         <img onClick={handleClick} className ="image" src="samosa.png"/>
       </div>
       <div className="Clicker">
-        <p>{clicks}</p>
+        <p>Clicks: {clicks}</p>
       </div>
 
       <div className="Multipliers">
         <div className="two">
           <h3>Double Stuffed</h3>
           <h5>2x Multiplier</h5>
-          <button onClick={() => handleMultiplier(2)} >10 samosas</button>
+          {twoFlag && <button onClick={doubler}>10 samosas</button>}
         </div>
 
         <div className="five">
           <h3>Party Pack</h3>
           <h5>5x Multiplier</h5>
-          <button onClick={() => handleMultiplier(5)}>100 samosas</button>
+          {fiveFlag && <button onClick={fiveX}>100 samosas</button>}
         </div>
 
         <div className="ten">
           <h3>Full Feast</h3>
           <h5>10x Multiplier</h5>
-          <button onClick={() => handleMultiplier(10)} >1000 samosas</button>
+          {tenFlag && <button onClick={tenX}>1000 samosas</button>}
         </div>
       </div>
 
